c90151c1996151e6146836390b2048772e19f087
Vivian Nadoti <vinadoti@gmail.com>
2021-11-11 20:44:14 +0000

    Primeira edição no código

--- app.py
+++ app.py
@@ -2,15 +2,17 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-tarefas = [
-    {"texto": "Estudar para a prova", "concluida": False},
-    {"texto": "Passear com o cachorro", "concluida": True},
-    {"texto": "Estudar mais para a prova", "concluida": False},
+URL = 'https://5000-cyan-marlin-ll559fu0.ws-us18.gitpod.io/'
+
+livros = [
+    {"id": 1, "titulo": "Estudar para a prova", "autor": "Machado de Assis", "categoria": "drama", "concluida": False},
+    {"id": 2, "titulo": "Passear com o cachorro","autor": "Machado de Assis", "categoria": "drama","concluida": True},
+    {"id": 3, "titulo": "Estudar mais para a prova","autor": "Machado de Assis", "categoria": "drama","concluida": False},
 ]
 
 @app.route('/')
 def index():
-    return render_template('index.html', lista=tarefas)
+    return render_template('index.html', lista=livros)
 
 @app.route('/create')
 def create():
@@ -18,11 +20,21 @@ def create():
 
 @app.route('/save', methods=['POST'])  # <form action="/save" method="POST">
 def save():
-    texto = request.form['texto']      # <input name="texto"/>
-    tarefa = { "texto": texto, "concluida": False }
-    tarefas.append(tarefa)
+    titulo = request.form['titulo']      # Entrada do título
+    autor = request.form['autor']  # Entrada do autor
+    categoria = request.form['categoria'] # Entrada da categoria
+    livro = { "titulo": titulo, "autor": autor, "categoria": categoria, "concluida": False }
+    livros.append(livro)
 
-    return redirect('https://5000-brown-lemur-kiu2j61j.ws-us18.gitpod.io/')
+    # Atribuindo o id            - Tentar fazer dinamico !!!
+    #ultimo= livros[-1]
+
+    return redirect(URL)
+
+@app.route('/delete/<id>')  
+def delete():
+    del livros
+    return redirect(URL)
 
 app.run(debug=True)
 
