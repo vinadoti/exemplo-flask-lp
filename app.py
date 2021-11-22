587421737eef42abe5e0d4b91a9b9ca579deb43b
Vivian Nadoti <vinadoti@gmail.com>
2021-11-22 21:48:26 +0000

    Atribuindo função update e finalizando o trabalho

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 #Sempre que acessar pela primeira vez o código, trocar a URL do index aqui:
-URL= 'https://5000-aquamarine-kiwi-44oo1n1g.ws-us18.gitpod.io'
+URL= 'https://5000-amaranth-crab-y4qk3gr1.ws-us17.gitpod.io'
 
 lista_livros = [
     {"id": 1, "titulo": "Orgulho e Preconceito", "autor": "Jane Austen", "categoria": "Romance", "concluida": False},
@@ -55,20 +55,41 @@ def delete(id):
 # Implementando o search ---------------------------------------------------------------------------------------------------
 @app.route('/search', methods=['POST'])  # <form action="/search" method="POST">
 def search():
-    titulo_lista = [] # Criando nova lista para o resultado da pesquisa
+    busca_lista = [] # Criando nova lista para o resultado da pesquisa
     buscar = request.form['busca'] # Entrada do título
     if buscar > '':
         for livro in lista_livros: 
-            if buscar.lower() in livro['titulo'].lower():   # Realiza a busca com o título dos livros presentes na lista de livros
-                titulo_lista.append(livro)  # Criando nova lista com os resultados das buscas
-        return render_template('search.html', titulo_lista=titulo_lista)
+            if buscar.lower() in livro['titulo'].lower()  or buscar.lower() in livro['autor'].lower() or buscar.lower() in livro['categoria'].lower():   # Realiza a busca com o título, autor e categoria dos livros presentes na lista de livros
+                busca_lista.append(livro)  # Criando nova lista com os resultados das buscas
+        return render_template('search.html', busca_lista=busca_lista)
 
     return render_template('error.html') # Retornando página de erro
 
+# Implementando a tela de alteração  ---------------------------------------------------------------------------------------------------
+@app.route('/edit/<id>')   
+def edit(id):
+    for indice, livro in enumerate(lista_livros):
+        if livro["id"] == int(id):
+            return render_template('update.html', livro=livro)
+
+    return render_template('error.html') # Retornando página de erro
+    
+# Implementando o update  ---------------------------------------------------------------------------------------------------
+@app.route('/update/<id>', methods=['POST'])  # <form action="/update" method="POST">
+def update(id):
+    
+    titulo = request.form['titulo']      # Entrada do título
+    autor = request.form['autor']  # Entrada do autor
+    categoria = request.form['categoria'] # Entrada da categoria
+
+    #Percorrer a lista, verificar o 'id' e alterar os elementos
+    for indice, livro in enumerate(lista_livros):
+        if livro["id"] == int(id):
+            livro["titulo"] = titulo
+            livro["autor"] = autor
+            livro["categoria"] = categoria
+    
+    #Redirecionando para a página principal
+    return redirect(URL + '/' )
+
 app.run(debug=True)
-
-#@app.route('/update/<id>')
-
-
-# Implementar o DELETE!! (2,0 pontos)
-# Implementar uma pesquisa (3,0 pontos)
